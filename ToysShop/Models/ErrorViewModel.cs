57828d660ba72a43db43321b5b45a149ebb6ab5b
BorshuliakM <borshuliakm@gmail.com>
2023-06-12 18:58:00 +0300

    Add project files.

---
+++ ToysShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToysShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
