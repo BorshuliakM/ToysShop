b672c21cba2166cd61578d57d8c489761e41d605
BorshuliakM <borshuliakm@gmail.com>
2023-06-23 16:06:22 +0300

    .

--- Models/Toy.cs
+++ Models/Toy.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace ToysShop.Models
 {
@@ -37,6 +38,7 @@ namespace ToysShop.Models
 
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
+        [ValidateNever]
         public Category Category { get; set;}
        // public string ImageUrl { get; set; }
     }
