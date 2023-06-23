c831afcd50d987bfdf6fcca35038b55dabd211c7
BorshuliakM <borshuliakm@gmail.com>
2023-06-23 03:53:38 +0300

    DbConnection configured

--- ToysShop/Controllers/HomeController.cs
+++ ToysShop/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace ToysShop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        //private readonly <HomeController> _;
 
         public HomeController(ILogger<HomeController> logger)
         {
